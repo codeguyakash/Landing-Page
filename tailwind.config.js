10ca84762af7a0f26a5b15a660cde24e451f3062
Akash (codeguyakash) <codeguyakash@gmail.com>
2024-04-21 14:01:33 +0530

    navbar

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,15 @@
 module.exports = {
   content: ["./index.html"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#121212",
+        secondary: "#ffd600",
+      },
+      fontFamily: {
+        "default-font": ["Inter", "sans-serif"],
+      },
+    },
   },
   plugins: [],
 };
