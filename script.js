5e414754baf0495c884efd6ae6edc427d918a907
Akash (codeguyakash) <codeguyakash@gmail.com>
2024-04-27 10:56:42 +0530

    product sections add

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
-let timer = setInterval(updateCounter, 50);
+let timer = setInterval(updateCounter, 10);
 
 function updateCounter() {
   let counterElement = document.getElementById("counter1");
   let counterElement2 = document.getElementById("counter2");
   let counterElement3 = document.getElementById("counter3");
   let count = parseInt(counterElement.textContent);
-  if (count == 399) {
+  if (count == 414) {
     clearInterval(timer);
   }
   count++;
   counterElement.textContent = count;
-  counterElement2.innerText = count;
-  counterElement3.innerText = count;
+  counterElement2.innerText = count + 256;
+  counterElement3.innerText = count + 384;
 }
